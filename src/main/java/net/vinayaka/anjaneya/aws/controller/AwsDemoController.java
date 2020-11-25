761493a5f6faa8a0975f47e6904ffed90162b509
Vinayaka-Anjaneya <vinayaka1.anjaneya1@gmail.com>
2020-11-25 21:54:49 +0530

    changes method level

--- src/main/java/net/vinayaka/anjaneya/aws/controller/AwsDemoController.java
+++ src/main/java/net/vinayaka/anjaneya/aws/controller/AwsDemoController.java
@@ -1,12 +1,13 @@
 package net.vinayaka.anjaneya.aws.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AwsDemoController {
 
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String home(){
         return "Welcome to Aws first Deployment !!!!";
     }
